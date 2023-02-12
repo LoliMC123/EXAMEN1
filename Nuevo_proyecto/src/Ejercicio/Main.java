83984e6323f86844f93ca9116c0e4f16e937a698
EJEMPLO ENTORNO <118214731+LoliMC123@users.noreply.github.com>
2023-02-12 18:40:13 +0100

    Update Main.java

--- Nuevo_proyecto/src/Ejercicio/Main.java
+++ Nuevo_proyecto/src/Ejercicio/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	System.out.println("hola  me  llamo     ");
+		System.out.println("tu puta madre");
 
 	
 	System.out.println("hasta luego ");
