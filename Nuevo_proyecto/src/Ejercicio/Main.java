c0d7b800a3528e96700376c780bdef6238626901
LoliMC123 <dmarcas262@g.educaand.es>
2023-02-12 12:29:41 +0100

    nuevo proyecto con todo para que tenga el mismo contenido todo el repositorio

--- Nuevo_proyecto/src/Ejercicio/Main.java
+++ Nuevo_proyecto/src/Ejercicio/Main.java
@@ -3,8 +3,10 @@ package Ejercicio;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("hola me llamo ");
+	System.out.println("hola me llamo     ");
 
+	
+	System.out.println("hasta luego");
 	}
 
 }
